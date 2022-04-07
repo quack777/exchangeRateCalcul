85442fe455aad54672d4666f0f29d83a6a25336f
Mcdonalds Trump <whdmschd2@gmail.com>
2022-04-08 01:10:45 +0900

    Add: router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FirstCal from './pages/first/FirstCal';
 import SecondCal from './pages/second/SecondCal';
 
 const Router: FC = () => {
   return (
-    <div className="App">
-      <FirstCal />
-      <SecondCal />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FirstCal />}></Route>
+          <Route path="/second" element={<SecondCal />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
