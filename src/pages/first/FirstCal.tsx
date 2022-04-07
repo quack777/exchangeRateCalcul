fb2cb523a282fd65916070632db44f3c8efdb30c
Mcdonalds Trump <whdmschd2@gmail.com>
2022-04-08 03:55:43 +0900

    Add: api에서 환율 가져오고 보여주기

--- src/pages/first/FirstCal.tsx
+++ src/pages/first/FirstCal.tsx
@@ -1,25 +1,63 @@
-import React, { FC } from 'react';
+import axios from 'axios';
+import React, { ChangeEvent, FC, FormEvent, useEffect, useState } from 'react';
+
+type exchangeRateInfo = {
+  [index: string]: number;
+  USDKRW: number;
+  USDJPY: number;
+  USDPHP: number;
+};
 
 const FirstCal: FC = () => {
+  const [exchangeRateInfo, setExchangeRateInfo] = useState<exchangeRateInfo>();
+  const [curSelectCountry, setCurSelectCountry] = useState<string>('KRW');
+  const [curCountryExchangeRate, setCurCountryExchangeRate] = useState<number>();
+
+  useEffect(() => {
+    axios
+      .get('/data/exchangeRate.json')
+      .then((res) => {
+        setExchangeRateInfo(res.data.quotes);
+        setCurCountryExchangeRate(res.data.quotes.USDKRW);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const changeCurCountry = (e: ChangeEvent<HTMLSelectElement>) => {
+    const country = `USD${e.target.value}`;
+    if (exchangeRateInfo) {
+      setCurCountryExchangeRate(exchangeRateInfo[country]);
+    }
+    setCurSelectCountry(e.target.value);
+  };
+
+  const sendMoney = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <p>송금국가: 미국(USD)</p>
-      <div>
-        <p>수취국가:</p>
-        <select>
-          <option value="한국">한국</option>
-          <option value="일본">일본</option>
-          <option value="필리핀">필리핀</option>
-        </select>
-      </div>
-      <p>환율: 1 KRW/USD</p>
-      <div>
-        <p>송금액:</p>
-        <input type="number"></input>
-        <p>USD</p>
-      </div>
-      <button>Submit</button>
-    </form>
+    <div>
+      <form onSubmit={sendMoney}>
+        <p>송금국가: 미국(USD)</p>
+        <div>
+          <p>수취국가:</p>
+          <select onChange={changeCurCountry}>
+            <option value="KRW">한국(KRW)</option>
+            <option value="JPY">일본(JPY)</option>
+            <option value="PHP">필리핀(PHP)</option>
+          </select>
+        </div>
+        <p>
+          환율: {curCountryExchangeRate?.toLocaleString()} {curSelectCountry}/USD
+        </p>
+        <div>
+          <p>송금액:</p>
+          <input type="number"></input>
+          <p>USD</p>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
