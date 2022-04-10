2a6c7928b68782f4e80b8bbe492d0aa631f9eeec
Mcdonalds Trump <whdmschd2@gmail.com>
2022-04-10 22:45:21 +0900

    Add: map으로 동적 보여주기, clicked active

--- src/pages/second/SecondCal.tsx
+++ src/pages/second/SecondCal.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
+import styled from 'styled-components';
 
 interface countrysInfo {
   id: number;
@@ -34,29 +35,71 @@ const countrysInfo: countrysInfo[] = [
 
 const SecondCal: FC = () => {
   const [selectedStandardCountry, setSelectedStandardCountry] = useState<string>(countrysInfo[0].engCountry);
+  const [clickedCountry, setClickedCountry] = useState<string>(countrysInfo[1].engCountry);
+
+  const changeSelectedCountry = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
+    const selectedCountry = e.target.value;
+    setSelectedStandardCountry(selectedCountry);
+  };
+
+  const changeClickedCountry = (e: React.MouseEvent<HTMLDivElement>) => {
+    const elementTarget = e.target as Element;
+    console.dir(elementTarget.innerHTML);
+    setClickedCountry(elementTarget.innerHTML);
+  };
+
+  const acitveClickedCountry = (country: string): boolean => {
+    return country === clickedCountry ? true : false;
+  };
 
   return (
     <div>
       <section>
-        <input />
-        <select>
+        <input type="number" />
+        <select onChange={changeSelectedCountry}>
           {countrysInfo.map((country) => {
             return <option key={country.id}>{country.engCountry}</option>;
           })}
         </select>
       </section>
-      <section>
-        {countrysInfo.map((country) => {
-          return <div key={country.id}>{country.engCountry}</div>;
-        })}
+      <ExchangeRateInfoBox>
+        <CountrysBox>
+          {countrysInfo.map((country) => {
+            return country.engCountry !== selectedStandardCountry ? (
+              <CountryClickBox
+                key={country.id}
+                onClick={changeClickedCountry}
+                active={acitveClickedCountry(country.engCountry)}
+              >
+                {country.engCountry}
+              </CountryClickBox>
+            ) : null;
+          })}
+        </CountrysBox>
         <div>
           <p>CAD 2,000.00</p>
           <p>기준일:</p>
           <p>2022</p>
         </div>
-      </section>
+      </ExchangeRateInfoBox>
     </div>
   );
 };
+
+const ExchangeRateInfoBox = styled.section`
+  width: 150px;
+  height: 150px;
+`;
+
+const CountrysBox = styled.div`
+  display: flex;
+`;
+
+const CountryClickBox = styled.div<{ active: boolean }>`
+  border: 1px solid black;
+  background-color: ${({ active }) => active && 'red'};
+  border-bottom: ${({ active }) => active && '0px'};
+`;
 
 export default SecondCal;
