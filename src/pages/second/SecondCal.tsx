1ae0ff4e325b32e31f9b6a487ea0c505a9efea6e
Mcdonalds Trump <whdmschd2@gmail.com>
2022-04-10 21:55:42 +0900

    Add: 두 번째계산기 기본 마크업

--- src/pages/second/SecondCal.tsx
+++ src/pages/second/SecondCal.tsx
@@ -1,9 +1,60 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+
+interface countrysInfo {
+  id: number;
+  engCountry: string;
+}
+
+const countrysInfo: countrysInfo[] = [
+  {
+    id: 0,
+    engCountry: 'USD',
+  },
+  {
+    id: 1,
+    engCountry: 'CAD',
+  },
+  {
+    id: 2,
+    engCountry: 'KRW',
+  },
+  {
+    id: 3,
+    engCountry: 'HKD',
+  },
+  {
+    id: 4,
+    engCountry: 'JPY',
+  },
+  {
+    id: 5,
+    engCountry: 'CNY ',
+  },
+];
 
 const SecondCal: FC = () => {
+  const [selectedStandardCountry, setSelectedStandardCountry] = useState<string>(countrysInfo[0].engCountry);
+
   return (
     <div>
-      <p>두번째 계산기</p>
+      <section>
+        <input />
+        <select>
+          {countrysInfo.map((country) => {
+            return <option key={country.id}>{country.engCountry}</option>;
+          })}
+        </select>
+      </section>
+      <section>
+        {countrysInfo.map((country) => {
+          return <div key={country.id}>{country.engCountry}</div>;
+        })}
+        <div>
+          <p>CAD 2,000.00</p>
+          <p>기준일:</p>
+          <p>2022</p>
+        </div>
+      </section>
     </div>
   );
 };
