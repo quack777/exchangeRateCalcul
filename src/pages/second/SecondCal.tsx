2c58692809adcb421303cbeaeab686f7e6765e6c
조은총 <whdmschd2@gmail.com>
2022-04-11 04:18:44 +0900

    Add: api 호출, standard USD 환율 계산 로직

--- src/pages/second/SecondCal.tsx
+++ src/pages/second/SecondCal.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, FC, useState } from 'react';
+import axios from 'axios';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 interface countrysInfoType {
@@ -37,6 +38,20 @@ const SecondCal: FC = () => {
   const [selectedStandardCountry, setSelectedStandardCountry] = useState<string>(countrysInfo[0].engCountry);
   const [clickedCountry, setClickedCountry] = useState<string>(countrysInfo[1].engCountry);
   const [enterdedAmount, setEnterdedAmount] = useState<string>();
+  const [exchangeRateQuotes, setExchangeRateQuotes] = useState<{[key: string]: number}>();
+  const [calculatedAmount, setCalculatedAmount] = useState(0);
+
+  useEffect(() => {
+    const getExchangeRateInfoApi = () => {
+      axios.get("/data/data.json")
+        .then((res) => {
+          console.log(res.data);
+          setExchangeRateQuotes(res.data.quotes);
+        })
+        .catch((err) => console.log(err));
+    }
+    getExchangeRateInfoApi();
+  }, [])
 
   const changeSelectedCountry = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectedCountry = e.target.value;
@@ -49,6 +64,7 @@ const SecondCal: FC = () => {
   const changeClickedCountry = (e: React.MouseEvent<HTMLDivElement>) => {
     const elementTarget = e.target as Element;
     setClickedCountry(elementTarget.innerHTML);
+    // setEnterdedAmount("0");
   };
 
   const acitveClickedCountry = (country: string): boolean => {
@@ -56,18 +72,36 @@ const SecondCal: FC = () => {
   };
 
   const changeEnterdedAmount = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     const money: string = e.target.value;
     const removedCommaMoney = Number(money.replaceAll(",", ""));
     if(removedCommaMoney > 1000) {
       return setEnterdedAmount("1,000")
     }
     if(money === " " || removedCommaMoney < 0 || isNaN(removedCommaMoney)) {
-      console.log("유효하지 않음");
+      // console.log("유효하지 않음");
     } else {
       setEnterdedAmount(removedCommaMoney.toLocaleString());
     }
   }
+
+  const sendMoneyCalculate = () => {
+    if(exchangeRateQuotes) {
+      // console.log(selectedStandardCountry, clickedCountry);
+      // console.log(enterdedAmount, exchangeRateQuotes[`${selectedStandardCountry}${clickedCountry}`]);
+    }
+    if(exchangeRateQuotes) {
+      if(selectedStandardCountry === "USD") {
+        const exchangeRate = Number(exchangeRateQuotes[`${selectedStandardCountry}${clickedCountry}`].toFixed(2));
+        const removedCommaMoney = Number(enterdedAmount?.replaceAll(",", ''));
+        console.log(removedCommaMoney, exchangeRate);
+        setCalculatedAmount(Number((exchangeRate * removedCommaMoney)))
+      }
+    }
+  }
+
+  useEffect(() => {
+    sendMoneyCalculate();
+  }, [enterdedAmount])
 
   return (
     <div>
@@ -94,7 +128,8 @@ const SecondCal: FC = () => {
           })}
         </CountrysBox>
         <div>
-          <p>{clickedCountry} 2,000.00</p>
+          <p>{clickedCountry} {calculatedAmount.toLocaleString()}</p>
+          <p>2,000.00</p>
           <p>기준일:</p>
           <p>2022</p>
         </div>
